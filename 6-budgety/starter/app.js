0ce74bdfde7d58f2c4137bf56dfcdb9414a28985
DavidCertinergie <davidthurion@gmail.com>
2019-10-18 09:40:50 +0200

    :sparkles: Module creation

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,24 +1,49 @@
 // BUDGET CONTROLLER
-var budgetController = (function() {})();
+var budgetController = (function() {
+  // Code
+})();
 
 // UI CONTROLLER
-var UIController = (function() {})();
+var UIController = (function() {
+  var DOMStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, // inc or exp
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
+  };
+})();
 
 // GLOBAL CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMStrings();
+
   var ctrlAddItem = function() {
-    // Get field input data
-    // Add item to budget controller
-    // Add new item to UI
-    // Calculate the budget
-    // Display the budget to UI
-    console.log("coucou");
+    // Get the input data
+    var input = UICtrl.getInput();
+    console.log(input);
+    // Add item to the budget controller
+    // Add new item to the ui
+    // Calculate new budget
+    // Display new budget to the ui
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function(event) {
-    if (event.keycode === 13 || event.which === 13) {
+    if (event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
     }
   });
